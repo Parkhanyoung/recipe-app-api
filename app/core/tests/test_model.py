f60283a64593cd59b5613e4c3639eca9c1ef2d62
Parkhanyoung <80797824+Parkhanyoung@users.noreply.github.com>
2021-09-28 20:18:04 +0900

    Added recipe model

--- app/core/tests/test_model.py
+++ app/core/tests/test_model.py
@@ -45,7 +45,7 @@ class ModelTests(TestCase):
         self.assertTrue(user.is_staff)
 
     def test_tag_str(self):
-        """Test that tag string representation"""
+        """Test the tag string representation"""
         tag = models.Tag.objects.create(
             user=sample_user(),
             name='Vegan',
@@ -54,10 +54,21 @@ class ModelTests(TestCase):
         self.assertEqual(str(tag), tag.name)
 
     def test_ingredient_str(self):
-        """Test that ingredient string representation"""
+        """Test the ingredient string representation"""
         ingredient = models.Ingredient.objects.create(
             user=sample_user(),
             name='Cucumber'
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """Test the recipe string representation"""
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='Steak and mushroom sauce',
+            time_minutes=5,
+            price=5.00,
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
